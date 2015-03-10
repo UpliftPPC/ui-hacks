da3b7a000ea7558119f91a7173a0c8f424cbb0d1
adamdharrington <adamdharrington@gmail.com>
2015-03-10 08:15:12 +0000

    svg mapping

--- Petition/petition.js
+++ Petition/petition.js
@@ -1,18 +1,19 @@
 (function($, target){
-	var $pc = $('.js-petition .petition-counter'),
-		$bar = $pc.find('.bar'),
-		c = $bar.text();
+	$('article .js-petition .petition-counter').each(function(){
+		var $pc = $(this),
+			$bar = $pc.find('.bar'),
+			c = $bar.text();
 		
-	$pc.find('.petition-counter--title').text('Target '+target+' signatures');
-	
-	$pc.find('.petition-counter--start').text('0');
-	$pc.find('.petition-counter--middle').text(Math.round(target/2));
-	$pc.find('.petition-counter--target').text(target);
+		$pc.find('.petition-counter--title').text('Target '+target+' signatures');
+		
+		$pc.find('.petition-counter--start').text('0');
+		$pc.find('.petition-counter--middle').text(Math.round(target/2));
+		$pc.find('.petition-counter--target').text(target);
 
-	$bar.css({
-		'max-width': '100%',
-		'min-width': '7%',
-		'width': Math.floor((c/target)*100) + '%'
+		$bar.css({
+			'max-width': '100%',
+			'min-width': '7%',
+			'width': Math.floor((c/target)*100) + '%'
 		});
-	
+	});
 })(jQuery, 500)
